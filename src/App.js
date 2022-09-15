24ce7be3b27720550fdcf800d050e6e8c3f5e680
rossmorr1 <rossmorr97@gmail.com>
2022-09-15 09:28:31 +0100

    added conditional rendering for login button and created logout functionality

--- src/App.js
+++ src/App.js
@@ -4,21 +4,29 @@ import { Home } from './pages/home.js';
 import { Directions } from './pages/directions.js';
 import { SignUp } from './pages/signup.js';
 import { LogIn }  from './pages/login';
+import { Logout } from './pages/logout';
 import { Route, Routes } from 'react-router-dom'
+import { loginContext } from './appContext/Context'
+import { useState } from 'react';
 
 
 function App() {
 
+  const [loggedIn, setLoggedIn] = useState(false);
+
 
   return (
     <div className="App">
+    <loginContext.Provider value = {{loggedIn, setLoggedIn}}>
       <Navbar/>
+      {/* <h1>you are logged in</h1> */}
+
       <Routes>
         {/* EXAMPLE-- to have multiple components on the page, wrap them in a fragment in the element tag */}
         <Route path = "/example" element = {<><Home /><Directions /></>}></Route>
 
         <Route path = "/signup" element = {<><SignUp /></>}></Route>
-        <Route path = "/login" element = {<><LogIn /></>}></Route>
+        <Route path = "/login" element = {<LogIn />}></Route>
 
 
         {/* home page */}
@@ -26,7 +34,11 @@ function App() {
 
         {/* directions page */}
         <Route path = "/directions" element = {<Directions />}></Route>
+
+        <Route path = "/logout" element = {<Logout />}></Route>
       </Routes>
+    </loginContext.Provider>
+
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,21 +1,30 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { loginContext } from './appContext/Context'
+import { Logout } from './pages/logout';
 
-export function Navbar(){
+export function Navbar() {
+    const { loggedIn, setLoggedIn } = useContext(loginContext);
     return <nav>
         <div className='leftdiv'>
-        <img src="/logo.jpg" alt="qalogo" />
+            <img src="/logo.jpg" alt="qalogo" />
         </div>
 
         <div className='centerdiv'>
-        <Link className = 'link' to = "/">Home</Link>
-        <Link className = 'link' to = "/directions">Directions</Link>
-        <Link className = 'link' to = "/example">Example</Link>
+            <Link className='link' to="/">Home</Link>
+            <Link className='link' to="/directions">Directions</Link>
+            <Link className='link' to="/example">Example</Link>
         </div>
 
-        <div className='rightdiv'>
-        <Link className = 'link' to = "/login">Log in</Link>
-        <Link className = 'link' to = "/signup">Sign up</Link>
-        </div>
+
+        {loggedIn ? <div>
+            <Logout loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+        </div> :
+            <div className='rightdiv'>
+                <Link className='link' to="/login">Log in</Link>
+                <Link className='link' to="/signup">Sign up</Link>
+            </div>}
+
 
 
 
