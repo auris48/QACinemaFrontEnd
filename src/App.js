9be086ac7f9e25fe1d29eb5fb6cb290c815cb881
shahan306 <shahan306@gmail.com>
2022-09-16 14:28:18 +0100

    added contact form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import SearchBar from "./Components/SearchBar";
+import BookData from "./Data.json";
+import Contact from "./Components/Contact";
+
+import {Route, Routes } from "react-router-dom";
+
+//adding search and contact router
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      
+      <Routes>
+      <Route path = "/SearchBar" element = {<><SearchBar /></>}></Route>
+        <Route path = "/Contact" element = {<><Contact /></>}></Route>
+        </Routes>
+
     </div>
   );
 }
+
+//<Route path = "/SearchBar" element = {<><SearchBar /></>}></Route>   this is the router 
+
+//<SearchBar placeholder="Search for a movie..." data={BookData} />    this is the search bar data import
+
 
 export default App;
