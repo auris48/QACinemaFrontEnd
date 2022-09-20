aa2954217291c622add3c13011af5d3f9fc9f3ef
jackhulatt <jhulatt7@gmail.com>
2022-09-20 09:38:57 +0100

    Created About Page

---
+++ src/pages/about.js
@@ -0,0 +1,47 @@
+export function about(){
+    return <div>
+    
+    <h1 className="aboutPageHeading">About Us!</h1>
+    <div className="aboutPage">
+
+        <img alt = "cinema" src = "/cinemapic.jpg"></img>
+
+        <div className="aboutUsParagraph">
+            <h1>About Us</h1>
+        <div className="aboutText">
+        <div className="aboutLeft">
+        <div className="textBlock">
+        <br />
+        <p>This site was created by Team Bravo for the Final Project of the 22JuneEnable4 QA Tech Specialist Training Cohort</p> <br />
+        </div>
+        <div className="textBlock">
+        <p><strong>Product Owner</strong></p>
+        <br />
+        <p>Mohammed Ahmed</p> <br />
+        </div>
+        <div className="textBlock">
+        <p><strong>Scrum Master</strong></p>
+        <br />
+        <p>Ross Morrison</p> <br />
+        </div>
+        <div className="textBlock">
+        <p><strong>Team Members</strong></p>
+        <br />
+        <p>Shahan Shah<br></br> Aurimas Pilnikovas<br></br> Jack Hulatt</p> <br />
+        </div>
+        <div className="textBlock">
+        <p><strong>Contact Us</strong></p>
+        <br />
+        <p>If you have any questions at all about the use of this site, please feel free to use the contact methods or the contact forms listed on the "Contact Us" page</p> <br />
+        </div>
+        </div>
+        </div>
+        </div>
+
+
+    </div>
+    </div>
+    
+}
+
+
