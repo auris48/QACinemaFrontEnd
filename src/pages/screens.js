7096deebaebfeb23d899d2cd2b86bdb3042b48eb
jackhulatt <jhulatt7@gmail.com>
2022-09-20 12:15:09 +0100

    Created Screens page and uploaded seating plan

---
+++ src/pages/screens.js
@@ -0,0 +1,34 @@
+export function Screens(){
+    return <div>
+    
+    <h1 className="screenPageHeading">Our Screens</h1>
+    <div className="directionsPage">
+        <div className="directionsParagraph">
+        <div className="directionsText">
+        <div className="directionsLeft">
+        <div className="textBlock">
+            <h1>Standard Screen</h1>
+        <div className="textBlock">
+            <img alt = "Standard Screen" src = "/standardscreen.png"></img>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+
+
+        <div className="directionsRight">
+        <div className="textBlock">
+            <h1>Deluxe "Directors Box" Screen</h1>
+        <div className="textBlock">
+            <img alt = "Deluxe Screen" src = "/deluxescreen.png"></img>
+        </div>
+
+
+    </div>
+    </div>
+    </div>
+    </div>
+
+
+}
