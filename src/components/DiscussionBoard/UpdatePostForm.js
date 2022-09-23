ad2abf582ed825c5e48f1523608ad28719d9ad03
auris48 <auris48@gmail.com>
2022-09-23 14:13:04 +0100

    fixed alert on update post form

--- src/components/DiscussionBoard/UpdatePostForm.js
+++ src/components/DiscussionBoard/UpdatePostForm.js
@@ -49,7 +49,7 @@ export default function UpdatePostForm(props) {
             const check = checkInputs();
             e.preventDefault();
             if (check !== true) {
-              return alert(checkInputs());
+              return alert(check);
             } else {
               return props.handleSubmit(e, props._id, newPost);
             }
